162b5de5bba06b45687e7619b4d9179848c29446
duongnd <duong.tnhp8386@gmail.com>
2023-06-12 15:47:45 +0700

    refactor contract

--- contracts/interfaces/IVRFCoordinator.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-interface IVRFCoordinator {
-  function requestRandomness() external returns (uint256);
-  function addConsumer(address, uint256) external;
-  function removeConsumer(address) external;
-  function registerOracle(address, uint256[2] calldata) external;
-}
